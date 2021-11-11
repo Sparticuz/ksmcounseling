7540e9d3c25d2d393b194c3d9d8333da024853a3
Kyle <kyle@dazser.com>
2021-11-11 09:51:24 -0500

    Add image optimizer

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,40 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, cls, sizes) {
+  sizes = JSON.parse(sizes);
+  let metadata = await Image(src, {
+    widths: sizes,
+    formats: ["avif", "jpeg"],
+    outputDir: "./dist/assets/img",
+    urlPath: "/assets/img/"
+  });
+
+  let imageAttributes = {
+    alt,
+    class: cls,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function(eleventyConfig) {
   // Copy `css` to `_site/css`
   // Keeps the same directory structure.
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/assets");
-    return {
-      dir: {
-        input: "src",
-        output: "dist"
-      }
+
+  // Add Shortcode for Images
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
     }
-  };
+  }
+};
--- src/playtherapy/in-home.md
+++ src/playtherapy/in-home.md
@@ -2,7 +2,7 @@
 tags: playtherapy
 title: In-Home Play Therapy
 modalLink: 'portfolioModal3'
-image: assets/img/bruno-nascimento-eo11MS0FSnk-unsplash.jpg
+image: src/assets/img/bruno-nascimento-eo11MS0FSnk-unsplash.jpg
 permalink: false
 ---
 Play Therapy sessions conducted by a parent has been researched and proven to be more effective than delivered by a therapist! Our In-Home Play Therapy training empowers you, the parent, to build the relationship and create the environment needed for your child to get better.
